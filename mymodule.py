e698fb52ce06498988cefee022710ccbda5db619
Mike Ralphson <mike.ralphson@gmail.com>
2017-05-23 12:14:00 +0100

    Initial commit

---
+++ mymodule.py
@@ -0,0 +1,277 @@
+def searchApis():
+    return True
+
+
+def getApiTemplates():
+    return True
+
+
+def getOwnerApis():
+    return True
+
+
+def updateOwner():
+    return True
+
+
+def deleteApi():
+    return True
+
+
+def getApiVersions():
+    return True
+
+
+def saveDefinition():
+    return True
+
+
+def deleteCollaboration():
+    return True
+
+
+def getCollaboration():
+    return True
+
+
+def updateCollaboration():
+    return True
+
+
+def renameApi():
+    return True
+
+
+def saveApiDefinitionByTemplate():
+    return True
+
+
+def transferApi():
+    return True
+
+
+def deleteApiVersion():
+    return True
+
+
+def getDefinition():
+    return True
+
+
+def bumpApi():
+    return True
+
+
+def getApiComments():
+    return True
+
+
+def addApiComment():
+    return True
+
+
+def updateApiComments():
+    return True
+
+
+def deleteApiComment():
+    return True
+
+
+def updateApiComment():
+    return True
+
+
+def addApiCommentReply():
+    return True
+
+
+def deleteApiCommentReply():
+    return True
+
+
+def updateApiCommentReply():
+    return True
+
+
+def setApiCommentStatus():
+    return True
+
+
+def deleteDraftApi():
+    return True
+
+
+def getDraft():
+    return True
+
+
+def saveDraft():
+    return True
+
+
+def forkApi():
+    return True
+
+
+def compareApis():
+    return True
+
+
+def compareApisFromFile():
+    return True
+
+
+def getJsonDefinition():
+    return True
+
+
+def getYamlDefinition():
+    return True
+
+
+def searchDomains():
+    return True
+
+
+def getDomainTemplates():
+    return True
+
+
+def getOwnerDomains():
+    return True
+
+
+def getOwnerReferences():
+    return True
+
+
+def deleteDomain():
+    return True
+
+
+def getDomainVersions():
+    return True
+
+
+def saveDomainDefinition():
+    return True
+
+
+def renameDomain():
+    return True
+
+
+def transferDomain():
+    return True
+
+
+def deleteDomainVersion():
+    return True
+
+
+def getDomainDefinition():
+    return True
+
+
+def bumpDomain():
+    return True
+
+
+def getDomainComments():
+    return True
+
+
+def addDomainComment():
+    return True
+
+
+def updateDomainComments():
+    return True
+
+
+def deleteDomainComment():
+    return True
+
+
+def updateDomainComment():
+    return True
+
+
+def addDomainCommentReply():
+    return True
+
+
+def deleteDomainCommentReply():
+    return True
+
+
+def updateDomainCommentReply():
+    return True
+
+
+def setDomainCommentStatus():
+    return True
+
+
+def deleteDraftDomain():
+    return True
+
+
+def getDraftDomain():
+    return True
+
+
+def saveDraftDomain():
+    return True
+
+
+def forkDomain():
+    return True
+
+
+def saveDomainDefinitionByTemplate():
+    return True
+
+
+def getDomainJsonDefinition():
+    return True
+
+
+def getDomainYamlDefinition():
+    return True
+
+
+def getAvailablePlugins():
+    return True
+
+
+def removePluginConfiguration():
+    return True
+
+
+def getPlugins():
+    return True
+
+
+def addPluginConfiguration():
+    return True
+
+
+def updatePluginConfiguration():
+    return True
+
+
+def triggerPluginConfiguration():
+    return True
+
+
+def buildConfigurationSchema():
+    return True
+
+
+def getApiTokenByCredentials():
+    return True
+
+
+
